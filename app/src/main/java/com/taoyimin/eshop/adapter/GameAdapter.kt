7c2b415d3fc23564cb49af4e028462274d3fc192
taoyimin <1103995288@qq.com>
2019-05-10 17:39:35 +0800

    add click event

--- app/src/main/java/com/taoyimin/eshop/adapter/GameAdapter.kt
+++ app/src/main/java/com/taoyimin/eshop/adapter/GameAdapter.kt
@@ -4,18 +4,22 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.AdapterView
 import android.widget.TextView
 import com.taoyimin.eshop.R
 import com.taoyimin.eshop.bean.Game
+import com.taoyimin.eshop.i.OnItemClickListener
+import com.taoyimin.eshop.i.OnItemLongClickListener
 
 /**
  * Created by Tao Yimin on 2019/5/4.
  */
 class GameAdapter(
-        private val games: MutableList<Game>,
-        private val onItemClickListener: AdapterView.OnItemClickListener
-) : RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
+        private val games: ArrayList<Game>)
+    : RecyclerView.Adapter<GameAdapter.GameViewHolder>() {
+
+    private var onItemClickListener : OnItemClickListener? = null
+    private var onItemLongClickListener : OnItemLongClickListener? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
             GameViewHolder(LayoutInflater.from(parent.context)
                     .inflate(R.layout.adapter_item_game, parent, false))
@@ -27,13 +31,20 @@ class GameAdapter(
             games[position].run {
                 holder.titleText.text = title
             }
-            setOnClickListener {
-                onItemClickListener.onItemClick(null, it, holder.adapterPosition, holder.itemId)
-            }
+            setOnClickListener{onItemClickListener?.onItemClick(games[position])}
+            setOnLongClickListener{onItemLongClickListener!!.onItemLongClick(games[position], position)}
         }
     }
 
-    class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class GameViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val titleText: TextView = itemView.findViewById(R.id.text_title)
+    }
+
+    fun setOnItemClickListener(onItemClickListener: OnItemClickListener){
+        this.onItemClickListener = onItemClickListener
+    }
+
+    fun setOnItemLongClickListener(onItemLongClickListener: OnItemLongClickListener){
+        this.onItemLongClickListener = onItemLongClickListener
     }
 }
