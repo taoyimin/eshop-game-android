bff9c926cf33474112a231333ac5861dfdeae313
taoyimin <1103995288@qq.com>
2019-05-14 17:16:53 +0800

    add login activity and layout

---
+++ app/src/main/java/com/taoyimin/eshop/activity/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.taoyimin.eshop.activity
+
+import com.taoyimin.eshop.R
+
+/**
+ * Created by Tao Yimin on 2019/5/14.
+ */
+class LoginActivity: BaseActivity(){
+    override fun setContentViewId(): Int {
+        return R.layout.activity_login
+    }
+
+    override fun initView() {
+
+    }
+
+    override fun loadData() {
+
+    }
+
+}
